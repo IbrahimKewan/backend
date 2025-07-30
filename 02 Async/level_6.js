0e3b5f3d1b533f2944bdbf9faba1ccb2b7fc4f4b
ibrahimkewan0 <ibrahimkewan@outlook.de>
2025-07-30 14:22:09 +0200

    nochmal uebung

--- 02 Async/level_6.js
+++ 02 Async/level_6.js
@@ -1,13 +1,54 @@
+// const fs = require("fs").promises;
+// const axios = require("axios");
+
+// async function readCsvFile() {
+//     try {
+//         const res = await fs.readFile("products.csv", "utf-8");
+//         const lines = res.trim().split("\n");
+//         const header = lines[0].split(",");
+//         const products = lines.slice(1).map((line) => {
+//             const values = line.split(",");
+//             const obj = {};
+//             header.forEach((key, index) => {
+//                 obj[key.trim()] = values[index].trim();
+//             });
+//             return obj;
+//         });
+//         return products;
+//     } catch (err) {
+//         console.log("Fehler beim lesen von datei" + err);
+//     }
+// }
+
+// // produkte mit gültigen preis filtern
+// function filterProduct(obj) {
+//     return obj.filter((item) => item.price > 0);
+// }
+
+// async function main() {
+//     const data = await readCsvFile();
+//     const filteredData = filterProduct(data);
+//     filteredData.forEach((x) => {
+//         console.log(`${x.name} - €${x.price}`);
+//     });
+//     try {
+//         await axios.post("https://ibrahimkewan.de", filteredData);
+//     } catch (err) {
+//         console.log("kann nicht posten " + err);
+//     }
+// }
+// main();
+
 const fs = require("fs").promises;
 const axios = require("axios");
 
-async function readCsvFile() {
+async function readFileCSV() {
     try {
-        const res = await fs.readFile("products.csv", "utf-8");
-        const lines = res.trim().split("\n");
-        const header = lines[0].split(",");
+        const data = await fs.readFile("products.csv", "utf-8");
+        const lines = data.trim().split("\n");
+        const header = lines[0].trim().split(",");
         const products = lines.slice(1).map((line) => {
-            const values = line.split(",");
+            const values = line.trim().split(",");
             const obj = {};
             header.forEach((key, index) => {
                 obj[key.trim()] = values[index].trim();
@@ -16,25 +57,27 @@ async function readCsvFile() {
         });
         return products;
     } catch (err) {
-        console.log("Fehler beim lesen von datei" + err);
+        console.log("Fehler beim lesen von Datei ... " + err);
     }
 }
 
-// produkte mit gültigen preis filtern
 function filterProduct(obj) {
     return obj.filter((item) => item.price > 0);
 }
-
 async function main() {
-    const data = await readCsvFile();
-    const filteredData = filterProduct(data);
-    filteredData.forEach((x) => {
-        console.log(`${x.name} - €${x.price}`);
+    const data = await readFileCSV();
+    const filter = filterProduct(data);
+    console.log(
+        "---------------------------------------------------------------------------------"
+    );
+    filter.forEach((x) => {
+        console.log(
+            `#\t\tID:${x.id} \t\t name: ${x.name} \t\t preis:${x.price}\t\t#`
+        );
     });
-    try {
-        await axios.post("https://ibrahimkewan.de", filteredData);
-    } catch (err) {
-        console.log("kann nicht posten " + err);
-    }
+    console.log(
+        "---------------------------------------------------------------------------------"
+    );
 }
+
 main();
